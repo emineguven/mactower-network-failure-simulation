9184d3ce96ed9c36bcc3383e03ac351d9a60eec5
emineguven <eguven@biolapmac03.spelman.edu>
2016-03-15 15:56:37 -0400

    model fitting Weibull and Gompertz
    this is comparison of distribution and the curve

--- gompertz.vs.weibull.model.R
+++ gompertz.vs.weibull.model.R
@@ -3,20 +3,7 @@
 #If X= F^-1(U), then X is a random variable with cumulative density function F_X(x)=F
 #and then compare its fitting with Gompertz and Weibull model
 
- rgompertz = function(lambda,alpha, n){
-  x.uniform = runif(n)
-  #this inverse function is done based on F_X(x)=1-e^(-lambda.x) 
-  #then for every number u generated with a uniform random number genrator,
-  #x= F^-1(u) where F=F_X.F^-1(u)=-1/lambda.ln(1-u)
-  inverse.gomp.cumulative = function(lambda,alpha,y) {  (1/alpha)*log(1 - (alpha/lambda)*log(1-y)  ) }
-  x.gompertz = inverse.gomp.cumulative(0.01,0.2, x.uniform)
-  return(x.gompertz)
-}
-
-random_values_gompertz<-rgompertz(0.01,0.2,100)
-
-plot(random_values_gompertz, type="o",col="red")
-hist(random_values_gompertz,freq="FALSE")
+ 
 
  Weibull <- function(delta,beta,n)
  {
@@ -25,10 +12,11 @@ hist(random_values_gompertz,freq="FALSE")
    return(X)
  }
  
- random_values_Weibull <- Weibull(3000,2,100)
- par(new=T)
- plot(random_values_Weibull, type="o",col="blue")
- hist(random_values_Weibull)
+ 
+ #random_values_Weibull <- Weibull(3000,2,100)
+ #par(new=T)
+# plot(random_values_Weibull, type="o",col="blue")
+ #hist(random_values_Weibull)
  
  #check again; Let’s check if this is reasonable, by superimposing the plot of the density of a Weibull random
  #variable with the same parameters
@@ -40,7 +28,32 @@ hist(random_values_gompertz,freq="FALSE")
  t <- seq(from=0, to=max(X)*1.5, by=1)
  f_w <- beta/delta*(t/delta)^(beta-1)*exp(-(t/delta)^beta)
  lines(t,f_w,col="blue")
+ #par(new=T)
+ plot(t,f_w,col="blue",type="o")
  
+ #lambda>0 and alpha>1
+ Gompertz <-function(lambda,alpha,n)
+ {
+   U<-runif(n)
+   Y<- -(log(lambda)-log(lambda-log(1-U)*log(alpha)))/log(alpha)
+   return(Y)
+ }
+ 
+ random_values_Gompertz <- Gompertz(0.01,2,100)
+ plot(random_values_Gompertz, type="o",col="red")
+ 
+ #check again; Let’s check if this is reasonable, by superimposing the plot of the density of a Weibull random
+ #variable with the same parameters
+ n <- 100
+ lambda <- 0.01
+ alpha<-2
+ Y <- Gompertz(0.01,2,n)
+ hist(Y,freq=FALSE)
+ #hist(random_values_gompertz,freq="FALSE")
+ t <- seq(from=0, to=max(Y)*1.5, by=1)
+ f_g <- lambda*alpha^t*exp(-lambda*((alpha^t)-1)/log(alpha))
+ lines(t,f_g,col="blue")
+ plot(t,f_g,col="blue",type="o")
  
  
  
